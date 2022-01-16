b58d766faa753f55739456fe29f652477793ae88
SOURABHPATIL777 <83913206+SOURABHPATIL777@users.noreply.github.com>
2022-01-16 16:25:55 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,100 @@
+var buttonColours=["red","blue","green","yellow"];
+
+var gamePattern=[];
+
+var userClickedPattern=[];
+
+var started = false;
+var level=0;
+
+$(document).keypress(function(){
+  if(!started){
+    $("level-title").text("level"+level);
+    nextsequence();
+    started = true;
+  }
+});
+
+
+
+
+$(".btn").click(function(){
+
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel){
+
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    console.log("success");
+
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeOut(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+
+  else{
+    console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    $("#level-title").text("game over,click any key to restart");
+
+    setTimeOut(function(){
+      $("body").removeClass("game-over");
+    },200);
+
+    startOver();
+  }
+
+}
+
+
+
+function nextSequence(){
+
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("level"+level);
+
+  var randomNumber=Math.floor(Math.random() * 4);
+
+  var randomChosenColour=buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
+  var audio=new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+
+  setTimeOut(function(){
+    $("#"+currentColour).removeClass("pressed");
+  },100);
+
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
